ee1bb05a424a8f89c64e048f667ca950d1213fbf
mohamidTarek96 <moohamed.tarek@dafagate.com>
2025-10-24 17:23:28 +0300

    add the banner to nave bar and condition to show only in home page

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,13 +1,14 @@
 'use client';
 
-import roxLogo from '@img/logos-solana/rox-chain.svg';
 import Logo from '@img/logos-solana/light-explorer-logo.svg';
+import roxLogo from '@img/logos-solana/rox-chain.svg';
 import { useDisclosure } from '@mantine/hooks';
 import { useClusterPath } from '@utils/url';
 import Image from 'next/image';
 import Link from 'next/link';
-// import { useSelectedLayoutSegment, useSelectedLayoutSegments } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 import React, { ReactNode } from 'react';
+
 import { ClusterStatusButton } from './ClusterStatusButton';
 
 export interface INavbarProps {
@@ -16,54 +17,68 @@ export interface INavbarProps {
 
 export function Navbar({ children }: INavbarProps) {
     // const [navOpened, navHandlers] = useDisclosure(false);
-    const [_, navHandlers] = useDisclosure(false);
+    const [_, _navHandlers] = useDisclosure(false);
     const homePath = useClusterPath({ pathname: '/' });
+    const pathname = usePathname();
+    const isHomePage = pathname === '/' || pathname === homePath;
     // const supplyPath = useClusterPath({ pathname: '/supply' });
     // const inspectorPath = useClusterPath({ pathname: '/tx/inspector' });
     // const selectedLayoutSegment = useSelectedLayoutSegment();
     // const selectedLayoutSegments = useSelectedLayoutSegments();
 
     return (
-        <nav className="navbar navbar-expand-lg navbar-light">
-            <div className="navbar-container">
-                <div className="navbar-main-row">
-                    <div className="navbar-left-section">
-                        <Link href={homePath}>
-                            <Image alt="Rox Explorer" height={42} src={Logo} width={164} />
-                        </Link>
+        <div className="navbar-wrapper d-flex flex-column">
+            <nav className="navbar navbar-expand-lg navbar-light navbar-no-margin">
+                <div className="navbar-container">
+                    <div className="navbar-main-row">
+                        <div className="navbar-left-section">
+                            <Link href={homePath}>
+                                <Image alt="Rox Explorer" height={42} src={Logo} width={164} />
+                            </Link>
 
-                        <span className="navbar-price-badge">
-                            <Image alt="Rox Explorer" height={18} src={roxLogo} width={18} />
-                            <p className="navbar-price-text text-white">
-                                <span style={{ fontWeight: 600 }}> $ROX = $1.00</span>{' '}
-                                <span style={{ color: '#169452', fontWeight: 600 }}>0.00%</span> | Txn fee:{' '}
-                                <span style={{ fontWeight: 600 }} className="text-white">
-                                    0.000005 ROX
-                                </span>{' '}
-                                per signature
-                            </p>
-                        </span>
+                            <span className="navbar-price-badge">
+                                <Image alt="Rox Explorer" height={18} src={roxLogo} width={18} />
+                                <p className="navbar-price-text text-white">
+                                    <span style={{ fontWeight: 600 }}> $ROX = $1.00</span>{' '}
+                                    <span style={{ color: '#169452', fontWeight: 600 }}>0.00%</span> | Txn fee:{' '}
+                                    <span style={{ fontWeight: 600 }} className="text-white">
+                                        0.000005 ROX
+                                    </span>{' '}
+                                    per signature
+                                </p>
+                            </span>
+                        </div>
+
+                        <div className="navbar-right-section flex-shrink-0 ms-1">
+                            <a href="https://roxcustody.io/" target="_blank" rel="noopener noreferrer">
+                                <p className="navbar-link-text text-white">Rox Custody</p>
+                            </a>
+
+                            <a href="https://documentation.roxcustody.io" target="_blank" rel="noopener noreferrer">
+                                <p className="navbar-link-text text-white">Documentation</p>
+                            </a>
+
+                            <ClusterStatusButton />
+                        </div>
                     </div>
 
-                    <div className="navbar-right-section flex-shrink-0 ms-1">
-                        <a href="https://roxcustody.io/" target="_blank" rel="noopener noreferrer">
-                            <p className="navbar-link-text text-white">Rox Custody</p>
-                        </a>
+                    <div className="px-4 w-75">
+                        <p className="navbar-subtitle">Explorer RoxChain</p>
 
-                        <a href="https://documentation.roxcustody.io" target="_blank" rel="noopener noreferrer">
-                            <p className="navbar-link-text text-white">Documentation</p>
-                        </a>
-
-                        <ClusterStatusButton />
+                        <div className="navbar-children-container  d-none d-lg-block">{children}</div>
                     </div>
                 </div>
+            </nav>
 
-                <div className="px-4 w-75">
-                    <p className="navbar-subtitle">Explorer RoxChain</p>
-
-                    <div className="navbar-children-container  d-none d-lg-block">{children}</div>
+            {/* Home Page Banner */}
+            {isHomePage && (
+                <div className="home-banner">
+                    <Image alt="Rox Explorer" height={42} src={Logo} width={164} />
+                    <p className="text-white text-center mb-0 home-banner-text">
+                        The Institutional-Grade Digital-Asset Platform
+                    </p>
                 </div>
-            </div>
-        </nav>
+            )}
+        </div>
     );
 }
